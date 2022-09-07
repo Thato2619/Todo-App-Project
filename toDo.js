415e5f3658da285a2e2cc2d1bd330c520b19069e
Thato2619 <108408682+Thato2619@users.noreply.github.com>
2022-09-07 16:40:17 +0200

    Create a empty class named todoApp
    The class should consists of empty array named todoItems and empty constructor function

--- toDo.js
+++ toDo.js
@@ -3,7 +3,15 @@ const form = document.querySelector("[data-form]");
 const list = document.querySelector("[data-list]");
 const input = document.querySelector("[data-input]");
 
+//Create an empty object that contains empty array and constructor
+class todoApp {
+    todoItems = [];
+    constructor(){}
+
+}
+
 //add event-Listener in the form 
 form.addEventListener("submit" , (e) => {
     e.preventDefault(' ');
+
 })
