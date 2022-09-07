80a008424e76f42c0de12db7ee544621fb02cb54
Thato2619 <108408682+Thato2619@users.noreply.github.com>
2022-09-07 10:48:04 +0200

    add eventListener to form

--- toDo.js
+++ toDo.js
@@ -2,3 +2,8 @@
 const form = document.querySelector("[data-form]");
 const list = document.querySelector("[data-list]");
 const input = document.querySelector("[data-input]");
+
+//add event-Listener in the form 
+form.addEventListener("submit" , (e) => {
+    e.preventDefault(' ');
+})
