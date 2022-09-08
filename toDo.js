ec4716a3f4c453162e9e4d755c81c24b63b98df8
Thato2619 <108408682+Thato2619@users.noreply.github.com>
2022-09-08 20:00:57 +0200

    add functionality in adding complete button & delete

--- toDo.js
+++ toDo.js
@@ -12,4 +12,23 @@ button.addEventListener('click' , addTodo);
 function addTodo(event){
     //prevents form from submiiting unwillingly
     event.preventDefault();
+    //add todo(DIV)
+    const todoDiv = document.createElement("div");
+    todoDiv.classList.add("todo-item");
+    //create LI
+    const newTodo = document.createElement("li");
+    newTodo.innerText = 'hey';
+    newTodo.classList.add('todo-item');
+    todoDiv.appendChild(newTodo);
+    //add Complete button within the LI
+    const completeButton = document.createElement("button");
+    completeButton.innerHTML = '<i class="fas fa-check"></i>'; 
+    completeButton.classList.add("complete-btn");
+    todoDiv.appendChild(completeButton);
+
+    //add delete button within the LI
+    const deleteButton = document.createElement("button");
+    completeButton.innerHTML = '<i class="fas fa-trash"></i>'; 
+    completeButton.classList.add("complete-btn");
+    todoDiv.appendChild(deleteButton);
 }
