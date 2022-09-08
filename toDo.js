7efea3fa45877507495aacc729e30d0ea3099b5a
Thato2619 <108408682+Thato2619@users.noreply.github.com>
2022-09-08 19:03:57 +0200

    create addTodo function in toDo.js
    within toDo.html file, add div tag that has class of 'todo' with two buttons of delete, completed and 'li' tags.
    (To signify list)

--- toDo.js
+++ toDo.js
@@ -5,3 +5,11 @@ const list = document.querySelector(".dataList");
 
 // add event listeners
 button.addEventListener('click' , addTodo);
+
+
+
+//function of addTodo
+function addTodo(event){
+    //prevents form from submiiting unwillingly
+    event.preventDefault();
+}
